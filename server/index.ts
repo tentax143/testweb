ec671dc80e8fef19a94250c3595cc3165770d1b2
manish kumar <143683468+tentax143@users.noreply.github.com>
2025-05-10 16:45:21 +0530

    dep test

--- server/index.ts
+++ server/index.ts
@@ -59,8 +59,11 @@ app.use((req, res, next) => {
   // ALWAYS serve the app on port 5000
   // this serves both the API and the client
   const port = 5000;
-  server.listen(port, "127.0.0.1", () => {
-    log(`serving on http://127.0.0.1:${port}`);
+  server.listen({
+    port,
+    host: "0.0.0.0",
+    reusePort: true,
+  }, () => {
+    log(`serving on port ${port}`);
   });
-  
 })();
